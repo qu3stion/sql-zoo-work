ba3ec0afd2c52ecd68dcc0133fdd5e12b08732d6
Braun Payne <84481864+qu3stion@users.noreply.github.com>
2022-06-01 21:25:44 -0400

    Update zoo.js

--- zoo.js
+++ zoo.js
@@ -38,7 +38,7 @@ SELECT name FROM world WHERE name LIKE '%oo%';
 -- Find the countries that have three or more a in the name
 SELECT name FROM world WHERE name LIKE '%a%a%a%';
 
--- Find the countries that have "t" as the second character.
+-- Find the countries that have "t" as the second character
 SELECT name FROM world WHERE name LIKE '_t%';
 
 -- Find the countries that have two "o" characters separated by two others.
@@ -50,13 +50,13 @@ SELECT name FROM world WHERE name LIKE '____';
 
 -- SECTION 2 - SELECT FROM world
 
--- show the name, continent and population of all countries.
+-- show the name, continent and population of all countries
 SELECT name, continent, population FROM world;
 
 -- Show the name for the countries that have a population of at least 200 million
 SELECT name FROM world WHERE population > 200000000;
 
--- Give the name and the per capita GDP for those countries with a population of at least 200 million.
+-- Give the name and the per capita GDP for those countries with a population of at least 200 million
 SELECT name, gdp/population from WORLD WHERE population > 200000000;
 
 -- Show the name and population in millions for the countries of the continent 'South America'. Divide the population by 1000000 to get population in millions
@@ -68,10 +68,10 @@ SELECT name, population FROM world WHERE name = 'France' OR name = 'Germany' OR 
 -- Show the countries which have a name that includes the word 'United'
 SELECT name FROM world WHERE name LIKE '%United%';
 
--- Show the countries that are big by area or big by population. Show name, population and area.
+-- Show the countries that are big by area or big by population. Show name, population and area
 SELECT name, population, area FROM world WHERE area > 3000000 OR population > 250000000;
 
--- Exclusive OR (XOR). Show the countries that are big by area (more than 3 million) or big by population (more than 250 million) but not both. Show name, population and area.
+-- Exclusive OR (XOR). Show the countries that are big by area (more than 3 million) or big by population (more than 250 million) but not both. Show name, population and area
 SELECT name, population, area from WORLD
 WHERE (area <= 3000000 AND  population > 250000000) 
 OR (area > 3000000 AND population <= 250000000);
